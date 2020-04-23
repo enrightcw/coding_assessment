7c0649200754305396146dfbbe13fd169222cda6
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-04-23 14:54:19 -0400

    Styling on top bar and button mostly complete.  Working on the onclick events for starting the game.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var headerEl = document.querySelector(".header");
 var timerEl = document.querySelector(".timer");
 var mainEl = document.querySelector(".main");
 var startBtn = document.createElement("button")
-
+document.body.appendChild(startBtn);
+startBtn.setAttribute("style", "display: block; height: 60px; width: 80px; margin: auto");
+startBtn.innerHTML = "Start!" ;
 
 var secondsLeft = 10;
 var score = 0;
@@ -45,6 +47,7 @@ var questions = {
 
 //countdown timer function
 function countdown() {
+    button.style.display = 'none'; 
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timerEl.textContent = secondsLeft + " remaining.";
@@ -58,23 +61,27 @@ function countdown() {
 }
 
 
-//Score screen and inital log
+//Score screen and inital log of that user's score
+
 function scoreScreen() {
     timerEl.textContent = "Finished!";
 
-    //need a box, score display, user input box locked to 3 char if possible, a submit button, and then store in local storage array, to be used on a high score screen.
+    //need a box, score display, user input box locked to 3 char if possible, a submit button, and then store in local storage array, to be used on a high score screen, with a clear button, and a return/go back button.
   
 }
 
 //Start Button
-function start(button) {
-    var buttonVal = button.name,
-    textbox = document.getElementById('input_' + buttonVal);
-    textbox.value = "Play!" ;
+//clear start button, and then start timer, and start questions. 
+
+function startGame(event) {
+    event.preventDefault();
+   
+    
+
 }
-  
+
+//high scores screen
 
 
 
-
-start.addEventListener("click", countdown);
+startBtn.addEventListener("click", countdown);
